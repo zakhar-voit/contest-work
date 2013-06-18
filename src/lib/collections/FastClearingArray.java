751d6d7f762c2d7ab06d167569c82bea5604c6e2
zakhar-voit <zakharvoit@gmail.com>
2013-06-18 14:20:51 +0000

    Implemented array that supports clearing with O(1) asymptotic.

---
+++ src/lib/collections/FastClearingArray.java
@@ -0,0 +1,90 @@
+package lib.collections;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.RandomAccess;
+
+/**
+ * Array that supports operation clear with O(1) asymptotic.
+ *
+ * @author Zakhar Voit(zakharvoit@gmail.com)
+ */
+@SuppressWarnings("unused")
+public class FastClearingArray<T> implements Iterable<T>, RandomAccess, Cloneable {
+    int timer = 0;
+    final T defaultValue;
+    T[] array;
+    int[] lastUpdate;
+
+    private FastClearingArray(T[] array, int[] lastUpdate, T defaultValue, int timer) {
+        this.array = array.clone();
+        this.lastUpdate = lastUpdate.clone();
+        this.defaultValue = defaultValue;
+        this.timer = timer;
+    }
+
+    @SuppressWarnings("unchecked")
+    public FastClearingArray(int size, T defaultValue) {
+        array = (T[]) new Object[size];
+        lastUpdate = new int[size];
+        this.defaultValue = defaultValue;
+        clear();
+    }
+
+    public int getLength() {
+        return array.length;
+    }
+
+    private void checkIndex(int idx) {
+        if (idx < 0 || idx >= array.length)
+            throw new ArrayIndexOutOfBoundsException(idx);
+    }
+
+    public T get(int idx) {
+        checkIndex(idx);
+        if (lastUpdate[idx] < timer) {
+            return defaultValue;
+        }
+        return array[idx];
+    }
+
+    public void set(int idx, T value) {
+        checkIndex(idx);
+        lastUpdate[idx] = timer;
+        array[idx] = value;
+    }
+
+    public void clear() {
+        ++timer;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int pos = 0;
+
+            @Override
+            public boolean hasNext() {
+                return pos < getLength();
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext())
+                    throw new NoSuchElementException();
+                return get(pos++);
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @Override
+    @SuppressWarnings("all")
+    protected Object clone() throws CloneNotSupportedException {
+        return new FastClearingArray<T>(array, lastUpdate, defaultValue, timer);
+    }
+}
